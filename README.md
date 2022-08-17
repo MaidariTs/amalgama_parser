9c1940a8bbca00b9574280ab4a1eb2098ed80a24
MaidariTs <tmaidari@mail.ru>
2022-08-18 02:09:04 +0300

    first_commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # amalgama_parser
 Parsing song strings from the site amalgama-lab.com, with subsequent sending to the telegram bot
+
+```for developers.```
+- Клонировать репозиторий
+- Установить и активировать виртуальное окружение
+- Установить requirements
+- В файле parser.py в функции main() вставить ссылку на песню с сайта amalgama-lab.com
+- В файле telegram_bot.py вставить Телеграм-токен бота
+
+```for users```
+В телеграме найти бота "" и нажать /start
---
+++ parser.py
@@ -0,0 +1,59 @@
+import requests
+import lxml.html
+import os
+
+from os.path import expanduser
+
+HOME_DIR = expanduser("~")
+PARSER_FILE_PATH = f'{HOME_DIR}/._amalgama_files'
+
+
+def parse_name(url):
+    try:
+        api = requests.get(url)
+    except Exception as ex:
+        return f'{ex}, не удалось получить данные с url'
+    tree = lxml.html.document_fromstring(api.text)
+    name = tree.xpath(
+        '/html/body/div[7]/div[3]/div[2]/div[3]/div[1]/h2[1]/text()')
+    return name
+
+
+def parse(url):
+    try:
+        api = requests.get(url)
+    except Exception as ex:
+        return f'{ex}, не удалось получить данные с url'
+    tree = lxml.html.document_fromstring(api.text)
+    text_original = tree.xpath(
+        '//*[@id="click_area"]/div//*[@class="original"]/text()')
+    text_translate = tree.xpath(
+        '//*[@id="click_area"]/div//*[@class="translate"]/text()')
+
+    os.chdir(HOME_DIR)
+
+    if not os.path.isdir("._amalgama_files"):
+        os.mkdir("._amalgama_files")
+
+    os.chdir(PARSER_FILE_PATH)
+
+    with open(
+         "text.txt", "w", newline='', encoding='utf-8') as txt_file:
+        for i in range(len(text_original)):
+            txt_file.write(text_original[i])
+            txt_file.write(text_translate[i])
+
+    os.rename("text.txt", f"{parse_name(url)}.txt")
+    txt_file.close()
+    # 1. Написать проверку на уже существующее название файла
+    # 2. Некоторые песни без перехода на новую строку
+
+
+def main():
+    link = 'Вставьте ссылку на песню с сайта amalgama-lab.com'
+    parse(link)
+    parse_name(link)
+
+
+if __name__ == "__main__":
+    main()
